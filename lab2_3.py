7ddf7511dc5d4084860755ee980c2a03ff0ad66b
siamcodes <siamcodes@gmail.com>
2024-06-17 15:14:48 +0700

    update

--- lab2_3.py
+++ lab2_3.py
@@ -1,14 +1,15 @@
-score = int(input("รับค่าคะแนน:"))
-if score >=80 and score <=100:
-    print("เกรด A")
-elif score >=70 and score <=79:
-    print("เกรด B")
-elif score >=60 and score <=69:
-    print("เกรด C")
-elif score >=50 and score <=59:
-    print("เกรด D")
-elif score >=0 and score <=49:
-    print("เกรด F")
+kg = int(input("รับค่าน้ำหนัก:"))
+cm = int(input("รับค่าส่วนสูง:"))
+bmi = kg / pow(cm/100,2)
+print("ค่า BMI = %.2f" % bmi)
 
-else:
-    print("กรุณากรอกข้อมูล 0-100")
+if bmi < 18.50:
+    print("น้ำหนักน้อย/ผอม")
+elif bmi >=18.50 and bmi <=22.90:
+    print("ปกติ  สุขภาพดี")
+elif bmi >=23 and bmi <=24.90:
+    print("ท้วม ระดับ 1")
+elif bmi >=25 and bmi <=29.90:
+    print("อ้วน ระดับ 2")
+elif bmi >=30 :
+    print("อ้วนมาก ระดับ 2")
